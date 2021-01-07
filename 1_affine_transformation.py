42ce66575d2ef6109fa94319fce575653bbd42d8
naysok_win10 <ysok.ysok@gmail.com>
2021-01-07 21:34:51 +0900

    [add] Affine_Transformation

---
+++ 1_affine_transformation.py
@@ -0,0 +1,66 @@
+import time
+import math
+
+from contour_draw_3d import image_processing, util
+
+ut = util.Util()
+im = image_processing.ImageProcessing()
+
+
+################################################################################
+
+
+prj_name = "bunny_offset"
+
+
+### WINDOWS 10
+dir_path = "C:\\Users\\ysoky\\Documents\\Contour_Draw_3D\\"
+prj_path = dir_path + "_prj_\\" + prj_name + "\\"
+
+
+DIR_SRC = prj_path + "image_0_src\\"
+DIR_AFF = prj_path + "image_1_affine_transformation\\"
+
+IMAGE_SIZE = (800, 800)
+
+
+################################################################################
+
+
+time_0 = time.time()
+
+
+### Prepare Project Foloder
+ut.prepare_prj_dir(dir_path, prj_name)
+
+### Get File Count
+count = ut.get_file_count(DIR_SRC)
+
+### Define Matrix for Render
+mat = im.define_matrix_for_render(IMAGE_SIZE)
+# print("Matrix :", mat_)
+
+
+for i in range(count):
+
+    ### Format
+    index = "%04d"%(int(i))
+
+    src_ = DIR_SRC + "image_{}.png".format(index)
+    aff_ = DIR_AFF + "image_{}.png".format(index)
+
+
+    # if i == 420:
+    if i >= 0:
+        
+        img_eddited = im.run_transform(src_, IMAGE_SIZE, mat)
+        # img_eddited.show()
+        im.export_image(img_eddited, aff_)
+
+
+time_1 = time.time()
+
+
+################################################################################
+
+print("Time_01 : {}Sec".format(time_1 - time_0))
